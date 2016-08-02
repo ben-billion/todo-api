dee493530eb493c0162fb27203399e2fe6234f68
Benjamin Neal <bengneal3@gmail.com>
2016-08-02 15:39:08 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: "Billion",
+// 	age: 21
+// };
+
+// function updatePerson(obj){
+// 	obj = {
+// 		name: "Billion",
+// 		age: 18
+// 	};
+// 	obj.age = 18;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array Example
+var grades = [38, 10];
+
+function addGrades (gradesArr){
+	grades.push(55);
+	debugger;
+	//gradesArr = [12, 22, 99];
+}
+
+addGrades(grades);
+console.log(grades);
