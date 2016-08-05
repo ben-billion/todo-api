750fe09b16c9180f44ca42fd8d1677e04fbb298f
Benjamin Neal <bengneal3@gmail.com>
2016-08-05 01:16:06 -0700

    Fixed db.js syntax error

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
-var sequelize
+var sequelize;
 
 if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
